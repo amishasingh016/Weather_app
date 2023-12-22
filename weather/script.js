9b4960aab1505febf2cd18d7eafd71c6c6697a54
Amisha Singh <113181362+amishasingh016@users.noreply.github.com>
2023-12-22 19:22:23 +0530

    Add files via upload

---
+++ weather/script.js
@@ -0,0 +1,38 @@
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'd6220fb475msh259c96b8516d363p19d163jsn9a78106697fd',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+const getWeather = (city) => {
+    cityname.innerHTML = city
+
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+        .then(response => response.json())
+        .then((response) => {
+            console.log(response)
+
+            
+            temp.innerHTML = response.temp
+            temp2.innerHTML = response.temp
+            feels_like.innerHTML = response.feels_like
+            humidity.innerHTML = response.humidity
+            humidity2.innerHTML = response.humidity
+            min_temp.innerHTML = response.min_temp
+            max_temp.innerHTML = response.max_temp
+            wind_speed.innerHTML = response.wind_speed
+            wind_speed2.innerHTML = response.wind_speed
+            wind_degrees.innerHTML = response.wind_degrees
+            sunrise.innerHTML = response.sunrise
+            sunset.innerHTML = response.sunset
+        })
+        .catch(err => console.error(err));
+}
+submit.addEventListener("click", (e) => {
+    e.preventDefault()
+    getWeather(city.value)
+})
+
+getWeather("Delhi")
